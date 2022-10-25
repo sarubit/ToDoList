75241193d62408d93066a647f3c29f4c3965bdf2
sarubit <carlossarubiuni@gmail.com>
2022-10-25 14:04:17 -0300

    aijsjkaskj

---
+++ backend/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::post('register', 'UserController@store')->name('users.store');
+
+//Route::post('register', 'UserController@store');
+
+Route::post('register', 'App\Http\Controllers\UserController@store');
+
+//Route::post('register', [UserController::class, 'store']);
